979cdc49c028ab9dc85fb46f34f5d2a2ed3035c2
manny1892 <manny1892@gmail.com>
2022-07-21 17:38:40 +0300

    homework #1 commit-fix3

--- src/test/java/BankServiceTest.java
+++ src/test/java/BankServiceTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,9 +12,7 @@ class BankServiceTest {
 
     @BeforeAll
     static void setUpAll() {
-        // WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
-
+        WebDriverManager.chromedriver().setup();
     }
 
     @Test
