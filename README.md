e06962cd81d074c68de6e6ec630cf07f5c31f4c0
manny1892 <manny1892@gmail.com>
2022-07-21 13:35:22 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/pc87p21f46aul6t3/branch/master?svg=true)](https://ci.appveyor.com/project/manny1892/auto2-1/branch/master)
---
+++ src/test/java/BankServiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeAll;
+
+public class BankServiceTest {
+@BeforeAll
+
+
+    static void setUpAll(){
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+}
