375102171a1af8f0fb671f86b1b161c3df1cbadf
manny1892 <manny1892@gmail.com>
2022-07-21 19:43:08 +0300

    homework #1 commit-fix7

--- src/test/java/BankServiceTest.java
+++ src/test/java/BankServiceTest.java
@@ -28,7 +28,10 @@ class BankServiceTest {
 
     @BeforeEach
     void setUp() {
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
         driver = new ChromeDriver();
     }
 
